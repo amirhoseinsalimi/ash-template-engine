942eeef032f135c8dcac141397756b58f4b35301
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2023-01-07 14:36:17 +0330

    test: write test for `if` & `for`

--- test/asp.spec.ts
+++ test/asp.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import { expect } from 'chai';
 import { TemplateEngine } from '../src';
-import { dataNested, dataSimple } from '../examples/shared';
+import { dataNested, dataSimple, dataArray } from '../examples/shared';
 
 describe('ASP Syntax', () => {
   it('should compile simple objects', () => {
@@ -21,5 +21,48 @@ describe('ASP Syntax', () => {
         { dataNested },
       ),
     ).to.equal('This car has V8 with 400 horsepower, and 2 doors.');
+  });
+
+  it('should compile `if` statements when `true`', () => {
+    const compiler = new TemplateEngine('asp').compile;
+
+    expect(
+      compiler(
+        `<% if (data) {%>
+          <% "This should be displayed" %>
+          <% } else { %>
+          <% "This should not be displayed" %>
+        <% } %>`,
+        { data: true },
+      ).trim(),
+    ).to.equal('This should be displayed');
+  });
+
+  it('should not compile `if` statements when `false`', () => {
+    const compiler = new TemplateEngine('asp').compile;
+
+    expect(
+      compiler(
+        `<% if (data) {%>
+          <% "This should be displayed" %>
+          <% } else { %>
+          <% "This should not be displayed" %>
+        <% } %>`,
+        { data: false },
+      ).trim(),
+    ).to.equal('This should not be displayed');
+  });
+
+  it('should compile `for` loops', () => {
+    const compiler = new TemplateEngine('asp').compile;
+
+    expect(
+      compiler(
+        `<% for (let i = 0; i < dataArray.length; i += 1) {%>
+          <% dataArray[i].name %> is <% dataArray[i].age %> years old.
+          <% } %>`,
+        { dataArray },
+      ).trim().split('.').map((text) => text.trim()).filter((text) => text),
+    ).to.include.members(dataArray.map((person) => `${person.name} is ${person.age} years old`));
   });
 });
