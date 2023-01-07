0258b421a4955dada6d5edbfbfacc7324a452f03
Amir Hosein Salimi <a.salimi@karboom.io>
2023-01-07 14:37:16 +0330

    docs: re-write `for` example using a data object

--- examples/asp/for.ts
+++ examples/asp/for.ts
@@ -1,11 +1,12 @@
 import { TemplateEngine } from '../../src';
+import { dataArray } from '../shared';
 
-const template = `<% for (let i = 0; i < 10; i += 1) {%>
-    This is the index: <% i %>
+const template = `<% for (let i = 0; i < dataArray.length; i += 1) {%>
+    <% dataArray[i].name %> is <% dataArray[i].age %> years old.
     <% } %>`;
 
 const templateEngine = new TemplateEngine('asp');
 
-const result = templateEngine.compile(template, {});
+const result = templateEngine.compile(template, { dataArray });
 
 console.log(result);
